9deb4dc8275c6c9cbf708ec7b32df034c184b925
LindseyJorissen <Lindsey.jorissen@gmail.com>
2024-12-05 09:46:20 +0100

    optimised project

--- Projects/Higher Lower/Higher_Lower.py
+++ Projects/Higher Lower/Higher_Lower.py
@@ -1,20 +1,25 @@
 import random
-def hoger_lager_game():
-    random_number = random.randint(1,101)
-    user_choice = False
-    attempt = 8
-    print("The Number guessing game: Guess a number between 1 and 100")
-    while user_choice == False:
-        choice = int(input(f"{attempt} attempt(s) left"))
-        attempt -=1
-        if attempt == 0:
-            print("Lost!")
+def higher_lower_game():
+    random_number = random.randint(1, 100)
+    attempts = 8
+    print("\nThe Number Guessing Game: Guess a number between 1 and 100,\nor enter 'quit' to end the game. \n")
+    while attempts > 0:
+        choice = input(f"You have {attempts} attempt(s) left. Enter your guess: ")
+        if choice.lower() == "quit":
+            print("Thank you for playing, Bye!")
             break
+        if not choice.isdigit():
+            print("Please enter a valid number or 'quit' to exit.")
+            continue
+        choice = int(choice)
+        attempts -= 1
         if choice == random_number:
-            print(f"Good pick! The number was {random_number}")
+            print(f"Good pick! The number was {random_number}. You won!")
             break
         elif choice > random_number:
-            print("Lower")
+            print("Lower!")
         else:
-            print("Higher")
-hoger_lager_game()
+            print("Higher!")
+        if attempts == 0:
+            print(f"You're out of attempts! The number was {random_number}. Better luck next time!")
+higher_lower_game()
