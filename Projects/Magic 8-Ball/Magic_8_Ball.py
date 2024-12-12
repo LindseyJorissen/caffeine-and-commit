efe6f3d1cc20c7e0eca42e1f89cf3aa9337795bd
LindseyJorissen <Lindsey.jorissen@gmail.com>
2024-12-12 22:59:24 +0100

    Added Magic 8-Ball

---
+++ Projects/Magic 8-Ball/Magic_8_Ball.py
@@ -0,0 +1,10 @@
+import random as r
+answers = []
+with open("answers.txt", "r") as inputfile:
+    for line in inputfile:
+        answers.append(line)
+input("Magic 8-Ball, Think of a question and press ent")
+num = r.randint(1,20)
+print(answers[num])
+
+
