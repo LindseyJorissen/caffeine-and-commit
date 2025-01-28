7875f27b9ae111f466dcd0134db0a655f77f4a6e
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-01-28 08:57:17 +0100

    Update shapes.py

--- Projects/shapes.py
+++ Projects/shapes.py
@@ -1,10 +1,8 @@
-def bottomleft_triangle():
-    height = int(input("Height: \n"))
+def draw_bottomleft_triangle(height):
     for i in range(height+1):
         print(i * "#\t")
         
-def bottomright_triangle():
-    height = int(input("Height: \n"))
+def draw_bottomright_triangle(height):
     for i in range(height):
         for j in range(i,height-1):
             print(" ",end="\t")
@@ -12,18 +10,38 @@ def bottomright_triangle():
             print("#",end="\t")
         print()
 
-def topleft_triangle():
-    height = int(input("Height: \n"))
+def draw_topleft_triangle(height):
     for i in range(height):
         for j in range(i,height):
             print("#", end="\t")
         print()
         
-def topright_triangle():
-    height = int(input("Height: \n"))
+def draw_topright_triangle(height):
     for i in range(height):
         for j in range(i):
             print(" ",end="\t")
         for k in range(i,height):
             print("#",end="\t")
+        print()       
+
+def draw_square(height):
+    for i in range(height):
+        for j in range(height):
+            print("#",end="  ")
+        print()
+
+def draw_rectangle(height,width):
+    for i in range(height):
+        for j in range(width):
+            print("#",end="  ")
+        print()
+        
+def draw_pyramid(height):       
+    for i in range(height):
+        for j in range(i,height-1):
+            print(" ",end=" ")
+        for j in range(i):
+            print("*",end=" ")
+        for j in range(i+1):
+            print("*",end=" ")
         print()
