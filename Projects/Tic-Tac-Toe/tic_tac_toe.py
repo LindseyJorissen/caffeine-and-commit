35bf63a7a251d9d29a01d8951b496f394e50875b
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-02-06 16:45:34 +0100

    finished tic-tac-toe

--- Projects/Tic-Tac-Toe/tic_tac_toe.py
+++ Projects/Tic-Tac-Toe/tic_tac_toe.py
@@ -9,7 +9,7 @@ def show_playboard(board):
     *_____|_____|_____*
 """)
 
-def do_turn(board,player_x_turns,player_o_turns,currentplayer_playerx):
+def do_turn(board, player_x_turns, player_o_turns, currentplayer_playerx):
     choice = check_choice(board)
     if currentplayer_playerx:
         player_x_turns.append(choice)
@@ -23,7 +23,7 @@ def do_turn(board,player_x_turns,player_o_turns,currentplayer_playerx):
 def check_choice(board):
     valid_choice = False
     while not valid_choice:
-        choice = int(input(""))
+        choice = int(input("Choose a box (0-8): "))
         if choice < 0 or choice > 8:
             print("Invalid input. Please choose a number between 0 and 8.")
         elif board[choice] == "X" or board[choice] == "O":
@@ -32,13 +32,16 @@ def check_choice(board):
             valid_choice = True
     return choice
 
-def check_win(player_o_turns,player_x_turns):
+def check_win(player_turns):
     winning_combos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]]
-    if winning_combos in player_x_turns or winning_combos in player_o_turns:
-        return True
+    for combo in winning_combos:
+        if all(cell in player_turns for cell in combo):
+            return True
+    return False
+
 
 def main():
     win = False
@@ -48,17 +51,21 @@ def main():
     player_o_turns = []
     print("Welcome to TIC TAC TOE! ")
     show_playboard(board)
-    print("Player 1, pick a box.\n")
     while not win:
+        if currentplayer_playerx:
+            print("Player X, it's your turn.")
+        else:
+            print("Player O, it's your turn.")
         currentplayer_playerx = do_turn(board, player_x_turns, player_o_turns, currentplayer_playerx)
-        if check_win(player_o_turns,player_x_turns):
-            print("Congratulations, You've won!")
-
+        if check_win(player_x_turns):
+            print("Congratulations, Player X has won!")
+            win = True
+        elif check_win(player_o_turns):
+            print("Congratulations, Player O has won!")
+            win = True
+        elif len(player_x_turns) + len(player_o_turns) == 9:
+            print("It's a tie!")
+            win = True
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
