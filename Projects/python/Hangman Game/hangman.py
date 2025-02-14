70953534ca104f6654e45975e54d0987ed5e6bd2
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-02-14 10:31:21 +0100

    Update hangman.py

--- Projects/python/Hangman Game/hangman.py
+++ Projects/python/Hangman Game/hangman.py
@@ -1,14 +1,6 @@
 import random as rand
 
-words = []
-word_display = []
-previous_guesses = []
-
-with open("word_list.txt", "r") as wordlist:
-  for word in wordlist:
-    words.append(word.strip())
-    
-def pick_random_word():
+def pick_random_word(words,word_display):
     random_word = rand.choice(words)
     for i in range(len(random_word)):
         word_display.append("_")
@@ -16,7 +8,6 @@ def pick_random_word():
     
 def do_turn(random_word,word_display,previous_guesses):
     right_letter = False
-  
     letter_choice = input("Pick a letter: ")
     if letter_choice in previous_guesses:
         letter_choice = input("Already guessed,pick another letter. ")
@@ -27,17 +18,24 @@ def do_turn(random_word,word_display,previous_guesses):
             word_display[i]= letter_choice
             right_letter = True
     if not right_letter:
-        previous_guesses.append(letter_choice)  
-      
-    print(word_display)
-    print(previous_guesses)  
+        previous_guesses.append(letter_choice) 
+    print("##hangman##")
+    print(" ".join(word_display))
+    print(f"Wrong Guesses: {' - '.join(previous_guesses)} \n")    
     return word_display,previous_guesses
 
-random_word,word_display = pick_random_word()
-while random_word.lower() != "".join(word_display):
-    word_display,previous_guesses = do_turn(random_word,word_display,previous_guesses)
-print(f"You won! "{random_word}"" was the word!")
-
-
-
-
+def play_hangman():
+    words = []
+    word_display = []
+    previous_guesses = []
+    
+    with open("word_list.txt", "r") as wordlist:
+        for word in wordlist:
+            words.append(word.strip())
+            
+    random_word,word_display = pick_random_word(words,word_display)
+    while random_word.lower() != "".join(word_display):
+        word_display,previous_guesses = do_turn(random_word,word_display,previous_guesses)
+    print(f"You won! '{random_word}' was the word!")
+    
+play_hangman()    
