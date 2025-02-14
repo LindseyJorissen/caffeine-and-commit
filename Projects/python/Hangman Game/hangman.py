d966206d8146a657ed91a28721ffb43c76d642d3
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-02-14 09:43:53 +0100

    Update hangman.py

--- Projects/python/Hangman Game/hangman.py
+++ Projects/python/Hangman Game/hangman.py
@@ -2,6 +2,7 @@ import random as rand
 
 words = []
 word_display = []
+previous_guesses = []
 
 with open("word_list.txt", "r") as wordlist:
   for word in wordlist:
@@ -13,26 +14,30 @@ def pick_random_word():
         word_display.append("_")
     return random_word,word_display    
     
-def do_turn(random_word,word_display):
+def do_turn(random_word,word_display,previous_guesses):
     right_letter = False
-    previous_guesses = []
   
     letter_choice = input("Pick a letter: ")
-  
-    if letter_choice in random_word:
+    if letter_choice in previous_guesses:
+        letter_choice = input("Already guessed,pick another letter. ")
+    if letter_choice in random_word.lower():
         right_letter = True
     for i in range(len(random_word)):
-        if random_word[i] == letter_choice:
+        if random_word.lower()[i] == letter_choice:
             word_display[i]= letter_choice
             right_letter = True
     if not right_letter:
-        previous_guesses.append(letter_choice)        
+        previous_guesses.append(letter_choice)  
+      
+    print(word_display)
+    print(previous_guesses)  
     return word_display,previous_guesses
 
 random_word,word_display = pick_random_word()
-word_display,previous_guesses = do_turn(random_word,word_display)
-print(word_display)
-print(previous_guesses)
+while random_word.lower() != "".join(word_display):
+    word_display,previous_guesses = do_turn(random_word,word_display,previous_guesses)
+print(f"You won! "{random_word}"" was the word!")
+
 
 
 
